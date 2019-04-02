6777c672db092d39b044e91da051b1c9aa1fe44c
Thomas Laber <laber.thomas@gmail.com>
2019-04-02 10:39:08 +0200

    Working on covr

--- R/fibo.R
+++ R/fibo.R
@@ -14,11 +14,6 @@ Fibonacci <- function(n){
   return(a)
 }
 
-# Hello, world!
-#
-# This is an example function named 'hello'
-# which prints 'Hello, world!'.
-#
 # You can learn more about package authoring with RStudio at:
 #
 #   http://r-pkgs.had.co.nz/
